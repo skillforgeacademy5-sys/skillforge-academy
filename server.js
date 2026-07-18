ab86b38e07d4e5dfd00c1af0978dfa898a3eca60
skillforgeacademy5-sys <skillforgeacademy5@gmail.com>
2026-07-18 07:47:14 +0000

    Temporarily disable Brevo

--- server.js
+++ server.js
@@ -41,12 +41,12 @@ const supabase = createClient(
 // Brevo
 // ======================================
 
-const apiInstance = new brevo.TransactionalEmailsApi();
-
-apiInstance.setApiKey(
-    brevo.TransactionalEmailsApiApiKeys.apiKey,
-        process.env.BREVO_API_KEY
-        );
+// Brevo temporarily disabled
+// const apiInstance = new brevo.TransactionalEmailsApi();
+// apiInstance.setApiKey(
+//     brevo.TransactionalEmailsApiApiKeys.apiKey,
+//     process.env.BREVO_API_KEY
+// );
 // ======================================
 // SkillForge Settings
 // ======================================
